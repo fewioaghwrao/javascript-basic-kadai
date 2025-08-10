169ca04ac8ae90213cfe74c574e3bf3974b24578
fewioaghwrao <nrk13959@gmail.com>
2025-08-11 07:51:37 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,11 @@
+// 変数(体重)
+let userBodyWeight;
+// 変数(身長)
+let userHeight;
+// 変数(BMI)
+let userBMI;
+// 変数の値の代入
+userBodyWeight = 68;
+userHeight=1.7;
+userBMI=userBodyWeight/(userHeight*userHeight);
+console.log(userBMI);
