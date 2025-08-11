b61bac62ec0f2a131f3b85c51cfb7a23ecd209e2
fewioaghwrao <nrk13959@gmail.com>
2025-08-11 10:58:31 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+//btnというidを持つHTML要素を取得し、定数に代入する
+ const Btn = document.getElementById('btn');
+ //textというidを持つHTML要素を取得し、定数に代入する
+ const Txt = document.getElementById('text');
+// HTML要素がクリックされたときにイベント処理を実行する
+ Btn.addEventListener('click', () => {
+  Txt.textContent = 'ボタンをクリックしました';
+ });
